9530ea7af7f9f991f9bb4b14622d8f4a0bdb4b04
navneetthakor <tnavneet8628@gmail.com>
2024-10-09 23:58:03 +0530

    strings completed in core_js

---
+++ 4_basic_JS/core_js/5_string.js
@@ -0,0 +1,50 @@
+// two ways to create string 
+
+// First - with String literals 
+let myName = "Navneet";
+
+// Second - with String Object constructor 
+let myNickName = "nk";
+    // -> but prefer to used String literals as they are optimized compare to second method 
+    // -> Because second method will create String object in heap, 
+    // while First will create non-mutable string on the stack
+
+// Following are the some useful methods of string 
+// -> when we apply method to string literal, it temporory creates String Object and after task complete, 
+// that object will be delected 
+
+// 1. charAt() 
+// returns character of specified index in string.
+console.log(myName.charAt(1)); // result => 'a'
+    // -> charAt() returns "" and [ ] returns 'undefined' for out of range indexes
+
+// 2. indexOf()
+// returns first index where, specified character will found in the string from LHS
+console.log(myName.indexOf('n')); // result => 3
+    // -> If character is not present then, result => -1 
+
+// 3. trim()
+// it remove white spaces from both end of the string 
+let trimExamp = "  hello      ";
+console.log(trimExamp.trim()); // result => "hello"
+
+// 4. substring()
+// it return substring
+console.log(myName.substring(1,3)); // result => 'av'
+    // -> ending index is optional
+    // -> ending indexed element will not be included 
+    // -> if end is greater then start then, they will be swaped 
+    // -> start or end is negative then, result => str.length + (start | end)
+
+// 5. replace()
+// first matching pattern will be replaced 
+console.log(myName.replace('e','E')); // result => 'NavnEet'
+
+// 6. replaceAll()
+// all matching patterns will be replaced 
+console.log(myName.replaceAll('e','E')); //result => 'NavnEEt'
+
+// 7. split()
+// it will return Array, of splited substring. 
+console.log(myName.split('v')); // result => ['Na', 'neet']
+    // -> we can specified number of time split can perform, as second parameter 
