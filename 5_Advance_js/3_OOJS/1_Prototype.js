b42798ab11467f39220343838ea6baf8362dd5a7
navneetthakor <tnavneet8628@gmail.com>
2024-10-26 23:18:04 +0530

    1_prototype.js completed

--- 5_Advance_js/3_OOJS/1_Prototype.js
+++ 5_Advance_js/3_OOJS/1_Prototype.js
@@ -1,3 +1,30 @@
+//---------- Understanding prototype --------
+// - Javascript is prototyped language not object-oriented 
+// - prototype is inheritance model of javascript 
+// - it have following features 
+        // -> Dynamic prototyping   (modify prototype at any point of time) 
+        // -> Prototype chaining 
+        // -> new keyword with Constructor function    (discussed in ./2_prototype_with_func.js)
+
+// --------- Let's see prototype with { string, Object and, Array} ----------
+// 1> string ----- 
+let str = "hello";
+
+String.prototype.sayName = function(){
+    console.log(`${this.toString()}`)
+}
+
+// if we call it before defining prototype then it will give error
+// prototype chaining is used here 
+str.sayName(); 
+
+// 2> Object -----
+Object.prototype.gn = "GoodNight";
+
+const obj = {};
+console.log(obj.gn);
+
+// one more way to add prototype in objects
 const base = {
     name: "Navneet",
     __proto__: {
@@ -15,3 +42,12 @@ base.myname();
 const child = Object.create(base);
 child.name = "Mayur";
 child.myname();
+
+
+// 3> Arrays -----
+const arr = [1,2,3];
+
+Array.prototype.getFirst = arr[0];
+
+console.log(arr.getFirst);
+console.log(arr.gn) // because object is perent of all 
