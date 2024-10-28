83fcae32ec5cc75b3a3983bbf18171b3fd37d5fe
navneetthakor <tnavneet8628@gmail.com>
2024-10-28 08:32:34 +0530

    Advance stuff added in 3_Object.js

--- 4_basic_JS/core_js/3_object.js
+++ 4_basic_JS/core_js/3_object.js
@@ -1,24 +1,42 @@
-// 3 ways to create object 
+// 3 ways to create object
 
 // first with object literals {}
 const myObj = {
-    "name is": "Navneet",
-    myid : "21CP031"
-}
+  "name is": "Navneet",
+  myid: "21CP031",
+};
 
-// second with Object constructor 
+// second with Object constructor
 const myObj2 = new Object();
 
-
-// third with any object as prototype 
+// third with any object as prototype
 const myObj3 = Object.create(myObj);
 
-// accessing values 
+// accessing values
 
 // with "."
 console.log(myObj.myid);
-    // - it has limitation as following will result in error 
-    // console.log(myObj."name is")
+// - it has limitation as following will result in error
+// console.log(myObj."name is")
 
 // better synatax  "[]"
 console.log(myObj["name is"]);
+
+// -------- Advance stuff -----------
+// -> How to make any property of object non editable?
+console.log("\n----- phase 2 -----");
+Object.defineProperty(myObj, "myid", {
+  writable: false,
+  enumerable: true,
+});
+
+myObj.myid = "125";
+myObj["name is"] = "nk";
+console.log(myObj);
+
+// -> how to make whole object non editable?
+console.log("\n----- phase 3 -----");
+Object.freeze(myObj);
+myObj.newProp = "hello";
+myObj["name is"] = "Meet";
+console.log(myObj);
