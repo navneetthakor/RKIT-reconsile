3f03d678592acd14c30813fd9295132bc1c40716
navneetthakor <tnavneet8628@gmail.com>
2024-11-11 21:42:14 +0530

    3_object.js in core_js updated

--- Basic_GUI/4_basic_JS/core_js/3_object.js
+++ Basic_GUI/4_basic_JS/core_js/3_object.js
@@ -40,3 +40,60 @@ Object.freeze(myObj);
 myObj.newProp = "hello";
 myObj["name is"] = "Meet";
 console.log(myObj);
+
+
+// how to get details about any property of the object?
+console.log("\n----- phase 3 -----");
+console.log(Object.getOwnPropertyDescriptor(myObj,"myid"));
+  // this returns object that contains descriptor for given key of the object
+  // result => 
+  /*  {
+      value: '21CP031',
+      writable: false,
+      enumerable: true,
+      configurable: false
+    }
+    */
+
+// -> all properties in above 3 phases are static properties of Object and that can't be inherted through prototype
+
+console.log(Object.getOwnPropertyDescriptors(myObj))
+  // result => 
+  /*
+  {
+    'name is': {
+      value: 'nk',
+      writable: false,
+      enumerable: true,
+      configurable: false
+    },
+    myid: {
+      value: '21CP031',
+      writable: false,
+      enumerable: true,
+      configurable: false
+    }
+  }
+  */
+
+// Object key have one attribute called as "configurable"
+// -> default it's true
+// -> once we set it to false, then except value and writable attribute we can't change anything else, for that key
+const obj = {
+  name: "navneet"
+}
+
+Object.defineProperty(obj,"name",{
+  configurable: false
+})
+
+//work fine
+Object.defineProperty(obj,"name",{
+  writable: false,
+})
+
+// through error 
+Object.defineProperty(obj,"name",{
+  enumerable: false,
+})
+
