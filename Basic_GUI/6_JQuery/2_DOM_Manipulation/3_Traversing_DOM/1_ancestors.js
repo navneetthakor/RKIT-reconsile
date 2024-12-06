d99ed39225f2162db659044b3a30fc490dfe7dc8
navneetthakor <tnavneet8628@gmail.com>
2024-12-06 21:49:41 +0530

    ancestors traversing completed

---
+++ Basic_GUI/6_JQuery/2_DOM_Manipulation/3_Traversing_DOM/1_ancestors.js
@@ -0,0 +1,27 @@
+// following are the methods to find ancestors strting from current element 
+
+// 1) parent()
+// -> it returns only direct parent of current element 
+// Syntax: 
+    // $(selector).parent();
+// Example: 
+    $('div').parent();
+
+// 2) parents()
+// -> it returns all the ancestor elements of the selected element, upto the document's root element 
+// Syntax:
+    //- $(selector).parents();
+    // - $(selector).parents(filter);
+// Example:
+    $('div').parents();
+        // return all the ancestors 
+    $('div').parents('div') 
+        // will return only those ancestors which are 'div'
+
+// 3) parentsUntil()
+// -> it returns all the element until specified element reached
+// Syntax:
+    // $(selector).parentsUntil();
+// Example:
+    $('div').parentsUntil('main');
+        // return all the ancestors of div upto 'main' ('main' not included)
