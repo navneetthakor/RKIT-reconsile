cd31fa53abd3fb87c457f25afd888bfb4ff9d3eb
navneetthakor <tnavneet8628@gmail.com>
2024-12-07 19:02:01 +0530

    DOM Manipulation completed

--- Basic_GUI/6_JQuery/2_DOM_Manipulation/3_Traversing_DOM/4_filtering.js
+++ Basic_GUI/6_JQuery/2_DOM_Manipulation/3_Traversing_DOM/4_filtering.js
@@ -11,4 +11,14 @@
 // 3) eq(index)
 // -> this returns element with specified index 
 // -> index starts with 0
-    $('div').eq(3) // this will return 4th element (which have index 3)
+    $('div').eq(3); // this will return 4th element (which have index 3)
+
+// 4) filter(criteria)
+// -> elements which matches criteria will be returned 
+// Example:
+    $('div').filter(".hello"); // div elements which have 'hello' class
+
+// 5) not(criterai)
+// -> elements which not matches criteria will be returned 
+// Example: 
+    $('div').not('.hello'); // div elements which not have 'hello' class
