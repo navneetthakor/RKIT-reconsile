b19034a5b55e994db4eae15157b0d8677555c784
navneetthakor <tnavneet8628@gmail.com>
2025-01-03 17:34:10 +0530

    cors package installed and global cos setup completed. controller level setup left

--- 2_Basic_API/3_web_dev/WebApplication1/WebApplication1/App_Start/WebApiConfig.cs
+++ 2_Basic_API/3_web_dev/WebApplication1/WebApplication1/App_Start/WebApiConfig.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Web.Http;
+using System.Web.Http.Cors;
 
 namespace WebApplication1
 {
@@ -10,6 +11,17 @@ namespace WebApplication1
         public static void Register(HttpConfiguration config)
         {
             // Web API configuration and services
+
+            //configuring cors 
+            //global cors configuration (lowest priority)
+            EnableCorsAttribute globalCors = new EnableCorsAttribute("*", "*", "*");
+            config.EnableCors(globalCors);
+
+            //setting cors for specific domain (higher priority then globalCors)
+            EnableCorsAttribute localCors = new EnableCorsAttribute("https://localhost:44300/", "*", "GET");
+            config.EnableCors(localCors);
+
+            // other is action and controller level cors
 
             // Web API routes
             config.MapHttpAttributeRoutes();
