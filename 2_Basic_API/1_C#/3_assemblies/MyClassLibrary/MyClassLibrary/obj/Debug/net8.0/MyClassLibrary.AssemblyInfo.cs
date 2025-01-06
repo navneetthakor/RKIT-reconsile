20091387373a6b89b69b5ae4169579a27ecc91f3
navneetthakor <tnavneet8628@gmail.com>
2025-01-06 14:49:49 +0530

    swagger added

--- 2_Basic_API/1_C#/3_assemblies/MyClassLibrary/MyClassLibrary/obj/Debug/net8.0/MyClassLibrary.AssemblyInfo.cs
+++ 2_Basic_API/1_C#/3_assemblies/MyClassLibrary/MyClassLibrary/obj/Debug/net8.0/MyClassLibrary.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MyClassLibrary")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+82985c3fad37abe3c4be0db1290342f004effeec")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
 [assembly: System.Reflection.AssemblyProductAttribute("MyClassLibrary")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MyClassLibrary")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- 2_Basic_API/1_C#/4_Collections/Collections/ConsoleApp1/obj/Release/net8.0/ConsoleApp1.AssemblyInfo.cs
+++ 2_Basic_API/1_C#/4_Collections/Collections/ConsoleApp1/obj/Release/net8.0/ConsoleApp1.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ConsoleApp1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d8ca2725cce5740f8578c9b09f6fbda9df38671d")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
 [assembly: System.Reflection.AssemblyProductAttribute("ConsoleApp1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ConsoleApp1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
