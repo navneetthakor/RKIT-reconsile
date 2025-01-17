55727440c810428d481a9991b3b52bfcb2b17c1e
navneetthakor <tnavneet8628@gmail.com>
2025-01-17 18:25:47 +0530

    AES started

---
+++ 3_Advance_API/2_C#_Advance/Tyeps_of_classes/10_Security_Cryptography/1_AES.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AdvanceAPI.Security_Cryptography.AES
+{
+    internal class Tester
+    {
+        public static void TestNow()
+        {
+
+        }
+
+        // Encrypts the plaintext using AES algorithm
+        public static string Encrypt(string plainText, string key)
+        {
+            using(Aes aes = Aes.Create()) 
+            {
+                //initializing required structure 
+                aes.Key = Encoding.UTF8.GetBytes(key);
+                aes.GenerateIV();
+
+
+            }
+            
+
+
+            return "";
+        }
+
+        // Decrypts the encrypted text using AES algorithm
+        public static string Decrypt(string cipherText, string key)
+        {
+
+
+            return "";
+        }
+    }
+}
