1ba8ccf9cf5e48f82ad6d296d89cd8de215ad462
navneetthakor <tnavneet8628@gmail.com>
2025-01-20 16:30:32 +0530

    updated main program for better designing in Database with C#

--- 3_Advance_API/4_DATABASE$_with_C#/4_DATABASE$_with_C#/Program.cs
+++ 3_Advance_API/4_DATABASE$_with_C#/4_DATABASE$_with_C#/Program.cs
@@ -19,38 +19,33 @@ namespace DBWC
                     conn.Open();
 
                     //Select query ----
-                    string selectQuery = "select * from pocoaewp01";
-                    using (MySqlCommand cmd = new MySqlCommand(selectQuery, conn))
-                    {
-                        using(MySqlDataReader reader = cmd.ExecuteReader())
-                        {
-                            while(reader.Read())
-                            {
-                                int id = reader.GetInt32("P01F01");
-                                string name = reader.GetString("P01F02");
-
-                                Console.WriteLine($"Id : {id}, Name : {name}");
-                            }
-                        }
-                    }
+                    Console.WriteLine("Before starting : ");
+                    Program.selectExecutor(conn);
+                    Console.WriteLine("\n\n");
 
                     ////Insert query ----
-                    //string insertQuery = "insert into pocoaewp01(P01F02,P01F03,P01F04,P01F05) values(" +
-                    //    "@P01F02,@P01F03,@P01F04,@P01F05);";
-                    //using (MySqlCommand cmd = new MySqlCommand(insertQuery, conn))
-                    //{
-                    //    //adding values 
-                    //    //cmd.Parameters.AddWithValue("@P01F01", 5);
-                    //    cmd.Parameters.AddWithValue("@P01F02", "Meet");
-                    //    cmd.Parameters.AddWithValue("@P01F03", "mJ2002");
-                    //    cmd.Parameters.AddWithValue("@P01F04", "meet@rkitsoftware.com");
-                    //    cmd.Parameters.AddWithValue("@P01F05", "4595123685");
+                    Console.WriteLine("Insert Query : ");
+                    string insertQuery = "insert into pocoaewp01(P01F02,P01F03,P01F04,P01F05) values(" +
+                        "@P01F02,@P01F03,@P01F04,@P01F05);";
+                    using (MySqlCommand cmd = new MySqlCommand(insertQuery, conn))
+                    {
+                        //adding values 
+                        //cmd.Parameters.AddWithValue("@P01F01", 5);
+                        cmd.Parameters.AddWithValue("@P01F02", "Meet");
+                        cmd.Parameters.AddWithValue("@P01F03", "mJ2002");
+                        cmd.Parameters.AddWithValue("@P01F04", "meet@rkitsoftware.com");
+                        cmd.Parameters.AddWithValue("@P01F05", "4595123685");
 
-                    //    int affectedRows = cmd.ExecuteNonQuery();
-                    //    Console.WriteLine("affectedRows : " + affectedRows);
-                    //}
+                        int affectedRows = cmd.ExecuteNonQuery();
+                        Console.WriteLine("affectedRows : " + affectedRows);
+                    }
+
+                    //Select query ----
+                    Program.selectExecutor(conn);
+                    Console.WriteLine("\n\n");
 
                     // update query ----
+                    Console.WriteLine("Update Query : ");
                     string updateQuery = " update pocoaewp01 set P01F03 = @P01F03 where P01F02 = @P01F02";
                     using(MySqlCommand cmd = new MySqlCommand(updateQuery, conn))
                     {
@@ -60,7 +55,12 @@ namespace DBWC
                         Console.WriteLine("affectedRows : " + affectedRows);
                     }
 
+                    //Select query ----
+                    Program.selectExecutor(conn);
+                    Console.WriteLine("\n\n");
+
                     // delete Query ----
+                    Console.WriteLine("Delete Query : ");
                     string deleteQuery = "delete from pocoaewp01 where P01F02 = @P01F02";
                     using (MySqlCommand cmd = new MySqlCommand(deleteQuery, conn))
                     {
@@ -68,6 +68,10 @@ namespace DBWC
                         int rowsAffected = cmd.ExecuteNonQuery();
                         Console.WriteLine($"{rowsAffected} row(s) deleted.");
                     }
+
+                    //Select query ----
+                    Program.selectExecutor(conn);
+                    Console.WriteLine("\n\n");
 
                 }
             }
@@ -77,6 +81,24 @@ namespace DBWC
             }
 
            
+        }
+
+        public static void selectExecutor(MySqlConnection conn)
+        {
+            string selectQuery = "select * from pocoaewp01";
+            using (MySqlCommand cmd = new MySqlCommand(selectQuery, conn))
+            {
+                using (MySqlDataReader reader = cmd.ExecuteReader())
+                {
+                    while (reader.Read())
+                    {
+                        int id = reader.GetInt32("P01F01");
+                        string name = reader.GetString("P01F02");
+
+                        Console.WriteLine($"Id : {id}, Name : {name}");
+                    }
+                }
+            }
         }
     }
 }
