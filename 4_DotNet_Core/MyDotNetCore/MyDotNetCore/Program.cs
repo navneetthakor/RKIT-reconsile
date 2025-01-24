b0bfbb31d5fa637a423c65ba2966f7b019e131f8
navneetthakor <tnavneet8628@gmail.com>
2025-01-24 18:48:12 +0530

    added program.cs + startup.cs

--- 4_DotNet_Core/MyDotNetCore/MyDotNetCore/Program.cs
+++ 4_DotNet_Core/MyDotNetCore/MyDotNetCore/Program.cs
@@ -7,32 +7,57 @@
  In .NET 6 and later, these responsibilities are combined into a more streamlined approach in a single Program.cs file.
  */
 
+#region template code
+//var builder = WebApplication.CreateBuilder(args);
 
-var builder = WebApplication.CreateBuilder(args);
+//// Add services to the container.
 
-// Add services to the container.
+//builder.Services.AddControllers();
+//// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+//builder.Services.AddEndpointsApiExplorer();
+//builder.Services.AddSwaggerGen();
 
-builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
-builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+//var app = builder.Build();
 
-var app = builder.Build();
-
-// Configure the HTTP request pipeline.
+//// Configure the HTTP request pipeline.
 
 
 
-if (app.Environment.IsDevelopment())
+//if (app.Environment.IsDevelopment())
+//{
+//    app.UseSwagger();
+//    app.UseSwaggerUI();
+//}
+
+//app.UseHttpsRedirection();
+
+//app.UseAuthorization();
+
+//app.MapControllers();
+
+//app.Run();
+#endregion
+
+using DemoProject;
+using Microsoft.AspNetCore.Hosting;
+
+public class Program
 {
-    app.UseSwagger();
-    app.UseSwaggerUI();
+    public static void Main(string[] args)
+    {
+        // Create a new WebApplicationBuilder instance
+        WebApplicationBuilder builder = WebApplication.CreateBuilder(args);
+
+        // Create a new instance of the Startup class, passing the application's configuration to it.
+        Startup startup = new Startup(builder.Configuration);
+
+        // Configuring Services
+        startup.ConfigureServices(builder.Services);
+
+        // Build the web application using the configured services
+        WebApplication app = builder.Build();
+
+        // Configure the application and its environment
+        startup.Configure(app, builder.Environment);
+    }
 }
-
-app.UseHttpsRedirection();
-
-app.UseAuthorization();
-
-app.MapControllers();
-
-app.Run();
