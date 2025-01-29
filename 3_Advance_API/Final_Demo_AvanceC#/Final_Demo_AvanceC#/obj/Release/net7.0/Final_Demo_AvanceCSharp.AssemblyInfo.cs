7ccca372d4583de8db84e303ff0ffc0c0c6bc714
navneetthakor <tnavneet8628@gmail.com>
2025-01-29 22:01:46 +0530

    implementing KFK

--- 3_Advance_API/Final_Demo_AvanceC#/Final_Demo_AvanceC#/obj/Release/net7.0/Final_Demo_AvanceCSharp.AssemblyInfo.cs
+++ 3_Advance_API/Final_Demo_AvanceC#/Final_Demo_AvanceC#/obj/Release/net7.0/Final_Demo_AvanceCSharp.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Final_Demo_AvanceCSharp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2be4241b905e673b4af9873f1eba2320f0af408f")]
 [assembly: System.Reflection.AssemblyProductAttribute("Final_Demo_AvanceCSharp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Final_Demo_AvanceCSharp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
