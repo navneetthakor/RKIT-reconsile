50ffb2b1f36b8a6ea5eaf0935a8e7989ddeeb643
navneetthakor <tnavneet8628@gmail.com>
2025-02-10 15:39:29 +0530

    Demo of editors in devextreme

---
+++ 5_Dev_Extream/1_Editors/Editors_in_VS/Editors_in_VS/Startup.cs
@@ -0,0 +1,73 @@
+﻿using Microsoft.OpenApi.Models;
+
+namespace Editors_in_VS
+{
+    public class Startup
+    {
+        IConfiguration configuration;
+        Startup(IConfiguration configuration)
+        {
+            this.configuration = configuration;
+        }
+
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddControllers();
+            services.AddEndpointsApiExplorer();
+
+            services.AddSwaggerGen();
+            services.AddSwaggerGen(c =>
+            {
+                // Add API key definition to Swagger
+                c.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme
+                {
+                    In = ParameterLocation.Header,
+                    Name = "Authorization",
+                    Type = SecuritySchemeType.ApiKey,
+                    Description = "Provide the API key in the header"
+                });
+
+                // Add security requirement to require the API key
+                c.AddSecurityRequirement(new OpenApiSecurityRequirement
+                 {
+                    {
+                        new OpenApiSecurityScheme
+                        {
+                            Reference = new OpenApiReference
+                            {
+                                Type = ReferenceType.SecurityScheme,
+                                Id = "Bearer"
+                            }
+                        },
+                        new string[] {}
+                    }
+                 });
+            });
+        }
+
+        public void Configure(WebApplication app, IWebHostEnvironment env)
+        {
+            if(env.IsDevelopment())
+            {
+                app.UseSwagger();
+                app.UseSwaggerUI();
+                app.UseDeveloperExceptionPage();
+            }
+
+            // for static file
+            app.UseStaticFiles();
+
+            //calling 
+            app.UseRouting(); 
+
+            // Redirect HTTP requests to HTTPS
+            app.UseHttpsRedirection();
+
+            // Map incoming requests to controller actions
+            app.MapControllers();
+
+            // End the request pipeline 
+            app.Run();
+        }
+    }
+}
